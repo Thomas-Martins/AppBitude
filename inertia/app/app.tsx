8fc45f0281d3936491df97ef2164f50d61676d89
Thomas Martins <thomas.martins1710@gmail.com>
2024-03-10 14:36:24 +0100

    chore: Adding UnoCSS packages

--- inertia/app/app.tsx
+++ inertia/app/app.tsx
@@ -1,6 +1,7 @@
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
 import { createInertiaApp } from '@inertiajs/react'
 import { hydrateRoot } from 'react-dom/client'
+import 'virtual:uno.css'
 import '../css/app.css'
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
